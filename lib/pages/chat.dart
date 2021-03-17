062b8b211ebcee17b6e95754960412e30df5d84f
huanent <huanent@icloud.com>
2021-03-17 10:48:39 +0800

    .

--- lib/pages/chat.dart
+++ lib/pages/chat.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_openvidu_demo/components/chat/ctrlBar.dart';
 import 'package:flutter_openvidu_demo/components/chat/errorDialog.dart';
