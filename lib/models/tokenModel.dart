1c7ddfb50e7dd228ca64f493f361f293e8b50a5f
huanent <huanent@icloud.com>
2021-04-12 14:24:16 +0800

    change server ip

--- lib/models/tokenModel.dart
+++ lib/models/tokenModel.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 
 class TokenModel extends ChangeNotifier {
-  String server = "http://117.28.236.91:5000";
+  //String server = "http://117.28.236.91:5000";
+  String server = "http://192.168.10.235:5000";
   String session = "abc";
   String userName = Platform.isAndroid ? "android_user" : "iphone_user";
 }
