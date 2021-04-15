bdd6932e3df8d65515ea13d5594205f56624193b
huanent <huanent@icloud.com>
2021-04-15 17:48:36 +0800

    change url

--- lib/models/tokenModel.dart
+++ lib/models/tokenModel.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 
 class TokenModel extends ChangeNotifier {
   //String server = "http://117.28.236.91:5000";
-  String server = "http://192.168.10.235:5000";
+  String server = "http://xmoffice.kooboo.cn:5000";
   String session = "abc";
   String userName = Platform.isAndroid ? "android_user" : "iphone_user";
 }
